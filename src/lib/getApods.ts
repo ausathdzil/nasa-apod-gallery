45bb6adcf8ed5ea6a1cf655365aad92af164a404
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-06-18 08:30:18 +0700

    feat: Revalidate data every hour

--- src/lib/getApods.ts
+++ src/lib/getApods.ts
@@ -3,7 +3,8 @@ import { secret, today, formattedStartDate } from '@/lib/constants';
 
 export async function getApods(): Promise<Apod[]> {
   const res = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${secret}&start_date=${formattedStartDate}&end_date=${today}&thumbs=True`
+    `https://api.nasa.gov/planetary/apod?api_key=${secret}&start_date=${formattedStartDate}&end_date=${today}&thumbs=True`,
+    { next: { revalidate: 3600 } }
   );
 
   if (!res.ok) {
